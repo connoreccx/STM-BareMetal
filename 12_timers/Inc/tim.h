c50e6d9f87fc4a5d5659cc6f6e2b494360fbeb1b
connoreccx <connor.ecclestone@rogers.com>
2025-12-01 16:37:45 -0500

    First timer lesson

---
+++ 12_timers/Inc/tim.h
@@ -0,0 +1,15 @@
+/*
+ * tim.h
+ *
+ *  Created on: Dec 1, 2025
+ *      Author: conno
+ */
+
+#ifndef TIM_H_
+#define TIM_H_
+
+void tim2_1hz_init(void);
+
+#define SR_UIF			(1U<<0)
+
+#endif /* TIM_H_ */
